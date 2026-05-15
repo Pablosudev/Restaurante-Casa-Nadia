efd1ba196524faaf5280ad5cd13856bfc0f09c4f
Pablosudev <pablo.losu.dev@gmail.com>
2026-05-15 21:59:29 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Restaurante-Casa-Nadia/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs'
+  }
 })
