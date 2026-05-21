a5c0c0b97ccc6fb2b42638a539a5ab3027d007b0
Pablosudev <pablo.losu.dev@gmail.com>
2026-05-21 08:24:36 +0200

    reqmerge

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { HashRouter } from 'react-router-dom'
-=======
->>>>>>> a108583dd779e44ade1189f059ac5670073bdbae
 import App from './App.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
     <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
 )
-=======
-    <App />
-  </StrictMode>,
-)
->>>>>>> a108583dd779e44ade1189f059ac5670073bdbae
